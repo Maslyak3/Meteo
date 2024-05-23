94385b84db0ebfba866227633a51b46245125bbb
Maslyak3 <maslyak3@gmail.com>
2024-05-23 13:03:30 +0300

    Api functionality implemented

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector('#search-form-input');
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector('#temperature');
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
+
+function searchCity(city) {
+  let apiKey = '965313106ef7fo435b5e0346a487b1at';
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector('#search-form-input');
+  let cityElement = document.querySelector('#city');
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+}
+
 let searchFormElement = document.querySelector('#search-form');
 searchFormElement.addEventListener('submit', handleSearchSubmit);
